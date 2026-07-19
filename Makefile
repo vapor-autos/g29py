f3a5487a58ace453fdfdd0e306494c942e2b7edd
Soulja Sim <sean@vapor.autos>
2026-07-19 12:48:20 -0400

    wip torque simulator

--- Makefile
+++ Makefile
@@ -13,7 +13,7 @@ install:
 	poetry install
 	pip install dist/*.whl
 test:
-	poetry run pytest -q tests/test_decode.py tests/test_events.py
+	poetry run pytest -q tests
 signin:
 	poetry config pypi-token.pypi ${PYPI_TOKEN}
 publish: build
---
+++ g29py/advanced/__init__.py
@@ -0,0 +1,13 @@
+from .torque import (
+    SteeringTorqueCommand,
+    SteeringTorqueConfig,
+    SteeringTorqueController,
+    accelerator_to_longitudinal_velocity_m_s,
+)
+
+__all__ = [
+    "SteeringTorqueCommand",
+    "SteeringTorqueConfig",
+    "SteeringTorqueController",
+    "accelerator_to_longitudinal_velocity_m_s",
+]
